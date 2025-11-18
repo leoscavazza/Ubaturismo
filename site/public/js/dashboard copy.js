986c10192b29121fd96c709b3b1cf2fe20964ecd
leoscavazza <leonardo.scavazza@sptech.school>
2025-11-18 20:26:40 -0300

    Adicionando os gráficos do chartjs na dashboard

--- site/public/js/dashboard copy.js
+++ site/public/js/dashboard copy.js
@@ -1,3 +1,56 @@
-const myChartPraia = document.getElementById(`myChartCanvasPraia`);
+const ctx1 = document.getElementById('myChart1');
 
-const myChartEvento = document.getElementById(`myChartCanvasEvento`);
+new Chart(ctx1, {
+    type: 'pie',
+    data: {
+        labels: [
+            'Red',
+            'Blue',
+            'Yellow'
+        ],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [300, 50, 100],
+            backgroundColor: [
+                '#020e4b',
+                '#083b14',
+                '#ffcc55ff'
+            ],
+            hoverOffset: 4
+        },]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false
+    }
+});
+
+
+// const myChartEvento = document.getElementById(`myChartCanvasEvento`);
+const ctx2 = document.getElementById('myChart2');
+
+new Chart(ctx2, {
+    type: 'pie',
+    data: {
+        labels: [
+            'Red',
+            'Blue',
+            'Yellow'
+        ],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [80, 200, 100],
+            backgroundColor: [
+                '#020e4b',
+                '#083b14',
+                '#ffcc55ff'
+            ],
+            hoverOffset: 4
+        },]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false
+    }
+});
+
